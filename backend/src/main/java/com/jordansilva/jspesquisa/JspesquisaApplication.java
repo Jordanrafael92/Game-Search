f61f4eb4f1d363d92b5a105a713b2fbf0ffd46b2
Jordan Silva <jordanrafael92@gmail.com>
2021-09-14 20:28:19 -0300

    Project created

---
+++ backend/src/main/java/com/jordansilva/jspesquisa/JspesquisaApplication.java
@@ -0,0 +1,13 @@
+package com.jordansilva.jspesquisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspesquisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JspesquisaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jordansilva/jspesquisa/JspesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jordansilva.jspesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JspesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
