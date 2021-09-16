5c158766b3c6f0234a8453fc0324183dd4fa8613
Jordan Silva <jordanrafael92@gmail.com>
2021-09-16 11:15:00 -0300

    Implemented DTO and Service

--- backend/src/main/java/com/jordansilva/jspesquisa/resources/GameResource.java
+++ backend/src/main/java/com/jordansilva/jspesquisa/resources/GameResource.java
@@ -8,17 +8,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.jordansilva.jspesquisa.entities.Game;
-import com.jordansilva.jspesquisa.repositories.GameRepository;
+import com.jordansilva.jspesquisa.dto.GameDTO;
+import com.jordansilva.jspesquisa.services.GameService;
 
 @RestController
 @RequestMapping(value = "/games")
 public class GameResource {
-
+	
+	@Autowired
+	private GameService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Game>> findAll() {
-		List<Game> list = gameRepository.findAll();
+	public ResponseEntity<List<GameDTO>> findAll() {
+		List<GameDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 		
